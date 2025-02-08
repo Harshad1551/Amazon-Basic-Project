d287a87c9169c8e5c1481f8e0277b676a679b6bb
Harshad1551 <harshadbhavake77@gmail.com>
2025-02-08 20:10:31 +0530

    Added all products

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,43 +1,7 @@
 // Below data represents the list (data structure:managed data)
-const productsIMG = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6',
-    rating: {
-        numbers : 87,
-        stars : 4.5
-    },
-    priceCents : 1090
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        numbers : 127,
-        stars : 4
-    },
-    priceCents : 2095
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        numbers : 56,
-        stars : 4.5
-    },
-    priceCents : 799
-},
-{
-  image: 'images/products/men-navigator-sunglasses-brown.jpg',
-  name: 'Men Brown Sunglasses',
-  rating: {
-      numbers : 100,
-      stars : 4
-  },
-  priceCents : 1000
-}
-];
+
 let productsHTML = '';
-productsIMG.forEach((EachProduct)=>{
+products.forEach((EachProduct)=>{
     productsHTML += `   <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -52,12 +16,12 @@ productsIMG.forEach((EachProduct)=>{
             <img class="product-rating-stars"
               src="images/ratings/rating-${EachProduct.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-               ${EachProduct.rating.numbers}
+               ${EachProduct.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${EachProduct.priceCents / 100}
+            $${(EachProduct.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
